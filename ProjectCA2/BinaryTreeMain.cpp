32317f677e5e3c95fd82a8b1b3d5e03befbfb19c
Shmithy16 <ben.arrowsmith17@gmail.com>
2024-12-01 23:42:12 +0000

    Adding part 3 csv and completing part 4

--- ProjectCA2/BinaryTreeMain.cpp
+++ ProjectCA2/BinaryTreeMain.cpp
@@ -4,11 +4,50 @@
 #include <sstream>
 #include "BinaryTree.h"
 #include "BSTNode.h"
-#include "TreeMapNode.h"
 #include "TreeMap.h"
 #include <string>
 
+
 using namespace std;
+
+struct pokemon
+{
+	string name,type;
+	int gen, dex;
+	bool evolves;
+	pokemon(string name,string type,int gen, int dex, bool evolves)
+	{
+		this->dex = dex;
+		this->type = type;
+		this->gen = gen;
+		this->evolves = evolves;
+		this->name = name;
+	}
+
+	bool operator<(pokemon& other)
+	{
+		return this->name < other.name;
+	}
+
+	bool operator>(pokemon& other)
+	{
+		return this->name > other.name;
+	}
+
+	bool operator==(pokemon& other)
+	{
+		return this->name == other.name;
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, pokemon& other)
+	{
+		return out << other.name << " " << other.type << " " << other.gen << " " << other.evolves << " " << other.dex;
+	}
+
+	pokemon()
+	{}
+
+};
 
 int main() {
 
@@ -19,7 +58,7 @@ int main() {
 
 	test.removeKey("a");
 
-	TreeMap<char, BinaryTree<string>> part2;
+	TreeMap<char, BinaryTree<string>> tMap;
 
 	ifstream fin("Part2Text.txt");
 	if (fin)
@@ -31,30 +70,78 @@ int main() {
 			stringstream  nextLine(text);
 			string word = "";
 			while (getline(nextLine, word, '\n')) {	
-				if (!part2.containsKey(word[0]))
+				if (!tMap.containsKey(word[0]))
 				{
 					BinaryTree<string> tree;
 					tree.add(word);
-					part2.put(word[0],tree);
+					tMap.put(word[0],tree);
 				}
 				else
 				{
-					part2.get(word[0]).add(word);
+					tMap.get(word[0]).add(word);
 				}
 				
 			}
 		}
-		cout << part2 << endl;
+		std::cout << tMap << endl;
 		fin.close();
 	}
 	else
 	{
-		cout << "Error opening file" << endl;
+		std::cout << "Error opening file" << endl;
+	}
+	TreeMap<string, pokemon> tMap2;
+	ifstream xl("ADSDataPage.csv");
+	string bad = "";
+	getline(xl, bad, '\n');
+	if (xl)
+	{
+		string name,type,gen,evolve,dex = "";
+
+		while (!xl.eof())
+		{
+
+			while (getline(xl, name, ',')) //https://stackoverflow.com/questions/16446665/c-read-from-csv-file
+			{
+				std::cout << "Name: " << name << " ";
+				getline(xl, type, ',');
+				std::cout << "Type: " << type << " ";
+				getline(xl, gen, ',');
+				int genI = stoi(gen);
+				std::cout << "Gen: " << genI << " ";
+				getline(xl, evolve, ',');
+				std::cout << "Evolves: " << evolve << " ";
+				getline(xl, dex);
+				int dexI = stoi(dex);
+				std::cout << "Dex: " << dexI << endl;
+				pokemon lilGuy(name, type, genI, dexI,evolve == "TRUE");
+				tMap2.put(name, lilGuy);
+			}
+		}
+
+		
+		string in;
+		std::cout << "Input Name: " << endl;
+		std::cin >> in;
+		xl.close();
+		cout << tMap2.get(in) << endl;;
+
+
+	}
+	else
+	{
+		std::cout << "Error opening file" << endl;
 	}
 
 	string m;
-	cout << "Input Letter: " << endl;
-	cin >> m;
-	cout << part2.get(m[0]) << endl;
+	std::cout << "Input Letter: " << endl;
+	std::cin >> m;
+	while (!tMap.containsKey(m[0])) 
+	{
+		std::cout << "Not valid letter" << endl;
+		std::cout << "Input Letter: " << endl;
+		std::cin >> m;
+	}
+	std::cout << tMap.get(m[0]) << endl;
 	return 0;
 }
